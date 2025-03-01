fadc4b8fe9b97f6cabb55038e143a84966e1a6ee
kempensp <kempensp@milwaukee.k12.wi.us>
2025-03-01 15:51:19 +0000

    Fixed debugging initial code

--- hsccinfo/routes/index.js
+++ hsccinfo/routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (process.env.CONSOLE_DEBUG){
+  if (process.env.CONSOLE_DEBUG=="true"){
     console.log("Debugging enabled!");
+  }
+  else{
+    console.log("Debugging disabled!");
   }
   res.render('index', { title: 'Express' });
 });
